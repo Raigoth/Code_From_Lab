7dfecb2fb6fee09bc2e1f36092654666891910c6
Raigoth <32273212+Raigoth@users.noreply.github.com>
2018-04-13 15:50:49 -0700

    Lab 2

---
+++ Lab2/Assets/Unit/Health.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour 
+{
+	float hitPoints;
+	float maxHitPoints;
+	float hitPointRegeneration;
+	float hitPointRecoveryTime;
+	Coroutine routine;
+
+	// Use this for initialization
+	void Awake() 
+	{
+		Unit unitInfo = gameObject.GetComponent<Unit>();
+		maxHitPoints = unitInfo.HitPoint;
+		hitPoints = maxHitPoints;
+		hitPointRegeneration = unitInfo.HitPointRegeneration;
+		hitPointRecoveryTime = unitInfo.HitPointRecoveryTime;
+	}
+
+	public float HitPoints
+	{
+		get { return hitPoints; }
+		set 
+		{
+			hitPoints += value;
+			if (value < 0)
+			{
+				Animator animation = gameObject.GetComponent<Animator>();
+
+				if (hitPoints > 0)
+				{
+					animation.SetTrigger("getHit");
+				}
+				else
+				{
+					gameObject.GetComponent<Combat>().enabled = false;
+					gameObject.GetComponent<Rigidbody>().useGravity = true;
+					gameObject.GetComponent<CapsuleCollider>().enabled = false;
+
+					animation.SetTrigger("isDead");
+
+					routine = StartCoroutine(Death(5.0f));
+				}
+			}
+			if (hitPoints > maxHitPoints)
+			{
+				hitPoints = maxHitPoints;
+			}
+		}
+	}
+
+	IEnumerator RegenerateHitPoints()
+	{
+		HitPoints = hitPointRegeneration;
+		yield return new WaitForSeconds(hitPointRecoveryTime);
+		routine = StartCoroutine(RegenerateHitPoints());
+	}
+
+	IEnumerator Death(float timeToDie)
+	{
+		yield return new WaitForSeconds(timeToDie);
+		Destroy(gameObject);
+	}
+
+	public void Stop()
+	{
+		StopCoroutine(routine);
+	}
+}
---
+++ Lab2/Assets/Unit/Unit.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Unit : MonoBehaviour
+{
+	[SerializeField] protected string unitName, description;
+	[SerializeField] protected bool isSelectable, isVulnerable, isMovable, isBuildable, hasAttack, hasVision, hasMana, isFlying;
+	[SerializeField] protected int hitPointRecoveryTime, armorPoint, costGold, costLumber, costPopulation, buildTime, attackGround, attackAir, manaPool;
+	[SerializeField] protected float hitPoint, hitPointRegeneration, movementSpeed, attackRange, attackRangeAir, attackSpeed, attackSpeedAir, visionRange, manaRegeneration;
+	[SerializeField] protected Armor armorType;
+	[SerializeField] protected Attack attackType;
+
+	public enum Armor
+	{
+		Unarmored,
+		Light,
+		Medium,
+		Heavy,
+		Fortified
+	};
+
+	public enum Attack
+	{
+		Slash,
+		Pierce,
+		Bludgeon,
+		Magic,
+		Siege
+	};
+
+	public string UnitName { get { return unitName; } set { unitName = value; } }
+	public string Description { get { return description; } set { description = value; } }
+
+	public bool IsSelectable { get { return isSelectable; } set { isSelectable = value; } }
+	public bool IsVulnerable { get { return isVulnerable; } set { isVulnerable = value; } }
+	public bool IsMovable { get { return isMovable; } set { isMovable = value; } }
+	public bool IsBuildable { get { return isBuildable; } set { isBuildable = value; } }
+	public bool HasAttack { get { return hasAttack; } set { hasAttack = value; } }
+	public bool HasVision { get { return hasVision; } set { hasVision = value; } }
+	public bool HasMana { get { return hasMana; } set { hasMana = value; } }
+	public bool IsFlying { get { return isFlying; } set { isFlying = value; } }
+
+	public int HitPointRecoveryTime { get { return hitPointRecoveryTime; } set { hitPointRecoveryTime = value; } }
+	public int ArmorPoint { get { return armorPoint; } set { armorPoint = value; } }
+	public int CostGold { get { return costGold; } set { costGold = value; } }
+	public int CostLumber { get { return costLumber; } set { costLumber = value; } }
+	public int CostPopulation { get { return costPopulation; } set { costPopulation = value; } }
+	public int BuildTime { get { return buildTime; } set { buildTime = value; } }
+	public int AttackGround { get { return attackGround; } set { attackGround = value; } }
+	public int AttackAir { get { return attackAir; } set { attackAir = value; } }
+	public int ManaPool { get { return manaPool; } set { manaPool = value; } }
+
+	public float HitPoint { get { return hitPoint; } set { hitPoint = value; } }
+	public float HitPointRegeneration { get { return hitPointRegeneration; } set { hitPointRegeneration = value; } }
+	public float MovementSpeed { get { return movementSpeed; } set { movementSpeed = value; } }
+	public float AttackRange { get { return attackRange; } set { attackRange = value; } }
+	public float AttackRangeAir { get { return attackRangeAir; } set { attackRangeAir = value; } }
+	public float AttackSpeed { get { return attackSpeed; } set { attackSpeed = value; } }
+	public float AttackSpeedAir { get { return attackSpeedAir; } set { attackSpeedAir = value; } }
+	public float VisionRange { get { return visionRange; }	set { visionRange = value; } }
+	public float ManaRegeneration { get { return manaRegeneration; } set { manaRegeneration = value; } }
+
+	public Armor ArmorType { get { return armorType; } set { armorType = value; } }
+	public Attack AttackType { get { return attackType; } set { attackType = value; } }
+
+
+}
